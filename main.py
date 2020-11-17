592835f0f87fea84aa624c8bf78bc3cf6e80aac7
achraffeydi <achreffaidiachref@gmail.com>
2020-11-17 01:32:01 +0100

    add graphs

--- main.py
+++ main.py
@@ -6,18 +6,5 @@ from solvers.taquin_solver_advanced import SlidingPuzzleAdvanced
 from solvers.taquin_solver_simple import SlidingPuzzleSimple
 
 
-def generate_random_positions(n) -> []:
-    initial_random_values = []
-    for x in range(n):
-        for y in range(n):
-            initial_random_values.append((x, y))
-    random.shuffle(initial_random_values)
-    return [(1, 0), (0, 1), (0, 2),
-            (1, 2), (0, 0), (2, 2),
-            (1, 1), (2, 0), (2, 1)]
-
-    return initial_random_values
-
-
-s = Simulator([(4, 4)], [20])
+s = Simulator([(3, 3),(4, 4)], [2,3,4,5,6,7,8,9,10])
 s.simulate()
--- solvers/Benchmarking/simulator.py
+++ solvers/Benchmarking/simulator.py
@@ -1,4 +1,7 @@
-import random
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import style
+import numpy as np
 
 from Solver import Solver
 from solvers.Benchmarking.random_puzzle_generator import generate_random_board
@@ -20,12 +23,47 @@ class Simulator:
                 random_values = generate_random_board(m, n, moves)
                 solver_simple = Solver(SlidingPuzzleSimple(random_values, m, n))
                 solver_advanced = Solver(SlidingPuzzleAdvanced(random_values, m, n))
-                solver_advanced.solve(True)
+                solver_advanced.solve(False)
                 solver_simple.solve(False)
                 output.append({
                     'size': (m, n),
-                    'number of moves': moves,
-                    'basic H': solver_simple.iterations,
-                    'advanced H': solver_advanced.iterations
+                    'number_moves': moves,
+                    'basicH': solver_simple.iterations,
+                    'advancedH': solver_advanced.iterations
                 })
-        print(output)
+        x1 = []
+        x2 = []
+        y1 = []
+        y2 = []
+
+        for value in output:
+            (m, n) = value['size']
+            x1.append(value['number_moves'])
+            x2.append(m * n)
+            y1.append(value['basicH'])
+            y2.append(value['advancedH'])
+        if len(self.number_of_moves) > 1 and len(self.sizes) > 1 :
+            fig = plt.figure(1)
+            ax = fig.add_subplot(111, projection='3d')
+            ax.plot_trisurf(x1, x2, y1, color='red', edgecolors='black', alpha=0.8)
+            ax.plot_trisurf(x1, x2, y2, color='blue', edgecolors='black', alpha=0.8)
+
+        else:
+            if len(self.number_of_moves) > 1:
+                f1 = plt.figure(1)
+                plt.plot(x1, y1, label='Basic Algo')
+                plt.plot(x1, y2, label='Advanced Algo')
+                plt.xlabel('number of changes')
+                plt.ylabel('number of iterations')
+                plt.title('How difficulty affects both algo ')
+                plt.legend()
+            if len(self.sizes) > 1:
+                f2 = plt.figure(2)
+                plt.plot(x2, y1, label='Basic Algo')
+                plt.plot(x2, y2, label='Advanced Algo')
+                plt.xlabel('Input size : m x n ')
+                plt.ylabel('number of iterations')
+                plt.title('how  input size affects both algo ')
+                plt.legend()
+
+        plt.show()
